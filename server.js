80a06a15ec2b6c5c1ecfb2996e4a3b1dbcc781fa
SHerlihy <65720475+SHerlihy@users.noreply.github.com>
2021-02-26 18:04:24 +0000

    update through patch working

--- server.js
+++ server.js
@@ -1,13 +1,24 @@
 const express = require("express");
+const bodyParser = require("body-parser");
 const cors = require("cors");
 const mongoose = require("mongoose");
 
 const app = express();
 
-var corsOptions = {
-  origin: "http://localhost:3000",
-  optionsSuccessStatus: 200, // some legacy browsers (IE11, various SmartTVs) choke on 204
-};
+//whole app
+app.use(bodyParser.urlencoded({ extended: true }));
+
+// parse application/json whole app, can limit to update later
+app.use(bodyParser.json());
+
+//enables all cors requests
+app.use(cors());
+
+//not using yet as all request routes authed by cors, may make other (!read,update) in b-end only
+// const corsOptions = {
+//   origin: "http://localhost:3000",
+//   optionsSuccessStatus: 200, // some legacy browsers (IE11, various SmartTVs) choke on 204
+// };
 
 const url = `mongodb+srv://steve_admin:test123@cluster0.fssow.mongodb.net/burgerIngredients?retryWrites=true&w=majority`;
 
@@ -27,7 +38,7 @@ const ingredient = new Ingredient({
   stock: 7,
 });
 
-app.get("/", cors(corsOptions), (req, res) => {
+app.get("/", (req, res) => {
   Ingredient.find((err, ingredients) => {
     if (err) {
       console.log(err);
@@ -37,15 +48,16 @@ app.get("/", cors(corsOptions), (req, res) => {
   });
 });
 
-app.post("/ingredients/:id", cors(corsOptions), (req, res) => {
+app.patch("/stock/:name", (req, res) => {
+  console.log(req.body.used);
   Ingredient.updateOne(
-    { name: req.params.id },
-    { stock: req.body.stock },
+    { name: req.params.name },
+    { $set: { stock: req.body.used } },
     (err) => {
       if (err) {
-        console.log(err);
+        res.send(err);
       } else {
-        console.log("Updated");
+        res.send(req.body);
       }
     }
   );
